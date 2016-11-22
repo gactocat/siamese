b88d48ceb9344a3fd545ecb4feabd7fe20ba513d
yamada-gakuto <yamada_gakuto@cyberagent.co.jp>
2016-11-22 14:08:55 +0900

    Install npm, webpack, babel, and eslint

---
+++ src/index.jsx
@@ -0,0 +1,3 @@
+require('./main.css');
+document.write('hello.');
+
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: {
+        html: "./src/index.html",
+        jsx: "./src/index.jsx",
+    },
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js",
+    },
+    module: {
+        loaders: [
+            { test: /\.html$/, loader: "file?name=[name].[ext]" },
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
+        ]
+    },
+    preLoaders: [
+            { test: /\.jsx?$/, exclude: /node_modules/, loader: "eslint-loader"}
+    ],
+    eslint: {
+            configFile: './.eslintrc',
+    },
+};
+
