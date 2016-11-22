46047c749b53cd18899e3bd540b4b6df7bb25801
yamada-gakuto <yamada_gakuto@cyberagent.co.jp>
2016-11-22 14:31:22 +0900

    Add react

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,9 @@
-require('./main.css');
-document.write('hello.');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.css';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
 
